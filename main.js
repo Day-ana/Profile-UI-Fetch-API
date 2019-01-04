07387e3e6490421566a0b5298e96aafa26594053
Dayana <daydesignz@gmail.com>
2019-01-04 01:02:01 -0800

    Added HTML 5 Video Player with Custom Controls, updated color scheme, updated images, updated JSON info, TODO: add functionality to other profiles, lorem ipsum images

--- main.js
+++ main.js
@@ -1,39 +1,84 @@
-let contactInfo = document.querySelector('.contact-info')
-let statsInfo = document.querySelector('.stats')
-let followingsInfo = document.querySelector('.followings')
+//Profile Elements
+const contactInfo = document.querySelector(".contact-info");
+const statsInfo = document.querySelector(".stats");
+const followingsInfo = document.querySelector(".followings");
 
-function renderProfile(user){
+//Video Elements
+const video = document.querySelector(".video");
+const progressBar = document.querySelector(".progress");
+const videoButton = document.querySelector("#control");
+
+//Video Functionality
+function togglePlayback() {
+  if (video.paused) {
+    videoButton.className = "pause";
+    video.play();
+  } else {
+    videoButton.className = "play";
+    video.pause();
+  }
+}
+
+//Progress Bar Update
+video.addEventListener("timeupdate", () => {
+  let progress = video.currentTime / video.duration;
+  progressBar.style.width = progress * 100 + "%";
+
+  //if Video Ends Reset play button
+  if (video.ended) {
+    videoButton.className = "play";
+  }
+});
+
+videoButton.addEventListener("click", () => {
+  togglePlayback();
+});
+
+//Profile Render
+function renderProfile(user) {
   //Adding Profiles components
-  let html =`<div class="row">
+  let html = `<div class="row">
               <div class="column">
-                <div class="profile-main"><img src="../assets/${user.avatarpath}"></div>
+                <div class="profile-main"><img src="../assets/${
+                  user.avatarpath
+                }"></div>
               </div>
               <div class="column right">
                 <ul>
-                  <li><span class="contact-label">Name</span>: ${user.firstName} ${user.lastName} </li>
-                  <li><span class="contact-label">Email</span>: ${user.email}</li>
-                  <li><span class="contact-label">Phone</span>: ${user.phone}</li>
-                  <li><span class="contact-label">Mobile</span>: ${user.mobile}</li>
-                  <li><span class="contact-label">Location</span>: ${user.location}</li>
+                  <li><span class="contact-label">Name</span>: ${
+                    user.firstName
+                  } ${user.lastName} </li>
+                  <li><span class="contact-label">Email</span>: ${
+                    user.email
+                  }</li>
+                  <li><span class="contact-label">Phone</span>: ${
+                    user.phone
+                  }</li>
+                  <li><span class="contact-label">Mobile</span>: ${
+                    user.mobile
+                  }</li>
+                  <li><span class="contact-label">Location</span>: ${
+                    user.location
+                  }</li>
                 </ul>
               </div>
           </div>`;
-    contactInfo.innerHTML += html;
+  contactInfo.innerHTML += html;
 }
 
-function renderStats(stats){
+function renderStats(stats) {
   //Adding stats components
-  let html = ``
-  stats.map((stat) =>{
-      html = `<div class="column section-container">
+  let html = ``;
+  stats.map(stat => {
+    html = `<div class="column section-container">
               <div class="stats-total">${stat.total}</div>
               <div class="stats-label">${stat.title}</div>
             </div>`;
-      statsInfo.innerHTML += html;
-  })
+    statsInfo.innerHTML += html;
+  });
 }
 
-function renderFollowings(data){
+function renderFollowings(data) {
   //Render Static Content
   followingsInfo.innerHTML += `
   <div class="row margin-10">
@@ -45,42 +90,51 @@ function renderFollowings(data){
   let html = ``;
 
   //Render rows with a .section-container class ie:bottom border unless it's the last row
-  data.followings.map((followee, index) =>{
-    if(index <= 3){
+  data.followings.map((followee, index) => {
+    if (index <= 3) {
       html = `<div id="${followee.followee.userId}" class="column">
                 <div class="row section-container">
-                  <img class="avatar-size" src="../assets${followee.followee.avatar}">
-                  <div class="column following">${followee.followee.userDisplayName}</div>
+                  <img class="avatar-size" src="../assets${
+                    followee.followee.avatar
+                  }">
+                  <div class="column following">${
+                    followee.followee.userDisplayName
+                  }</div>
                 </div>
               </div>`;
-            followingsInfo.innerHTML += html;
-      }else if(index === 4){
+      followingsInfo.innerHTML += html;
+    } else if (index === 4) {
       html = `<div id="${followee.followee.userId}" class="column">
                 <div class="row">
-                  <img class="avatar-size" src="../assets${followee.followee.avatar}">
-                  <div class="column following">${followee.followee.userDisplayName}</div>
+                  <img class="avatar-size" src="../assets${
+                    followee.followee.avatar
+                  }">
+                  <div class="column following">${
+                    followee.followee.userDisplayName
+                  }</div>
                 </div>
               </div>`;
-            followingsInfo.innerHTML += html;
+      followingsInfo.innerHTML += html;
     }
-  })
+  });
   //render see all last row
   followingsInfo.innerHTML += `<div class="row see-all">
                                   <div class="column"> SEE ALL</div>
                                 </div>`;
 }
 
-fetch('data.json')
-  .then((response) =>{
+fetch("data.json")
+  .then(response => {
     return response.json();
-  }).then((data) =>{
+  })
+  .then(data => {
     //Render components
 
-    renderProfile(data.data.userInfo)
-    renderStats(data.data.stats)
-    renderFollowings(data.data)
-
-  }).catch((error) =>{
-    contactInfo.innerHTML = "Please run a local server to view Page."
-    reject(error)
-})
+    renderProfile(data.data.userInfo);
+    renderStats(data.data.stats);
+    renderFollowings(data.data);
+  })
+  .catch(error => {
+    contactInfo.innerHTML = "Please run a local server to view Page.";
+    reject(error);
+  });
